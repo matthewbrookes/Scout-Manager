d47fb682607b42599049ccc6f70d7a81d7beabdb
Matthew Brookes <mbrookes1304@gmail.com>
2014-08-06 15:16:48 +0100

    Created badge class
    This class will represent each entity in badge table on database

---
+++ src/Badge.java
@@ -0,0 +1,64 @@
+/**
+ * This class represents a record in the Badges table from the database
+ * @author Matthew Brookes
+ */
+public class Badge {
+	//Declare variables which will be used in class
+	private String name;
+	private byte requirementsNeeded;
+	private String[] requirements = new String[10];
+	/**
+	 * Empty constructor as the variables will all be assigned 
+	 * using relevant setters
+	 */
+	public Badge() {}
+	
+	
+	/**
+	 * Returns the name of the badge
+	 * @return String The badge name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * Set the name of the badge
+	 * @param name The badge name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	/**
+	 * Returns the number of requirements needed to achieve badge
+	 * @return byte The number of requirements needed to achieve badge
+	 */
+	public byte getRequirementsNeeded() {
+		return requirementsNeeded;
+	}
+	/**
+	 * Sets the number of requirements needed to achieve badge
+	 * @param requirementsNeeded The number of requirements needed to achieve badge
+	 */
+	public void setRequirementsNeeded(byte requirementsNeeded) {
+		this.requirementsNeeded = requirementsNeeded;
+	}
+	
+	
+	/**
+	 * Returns the requirements of the badge as an array of strings
+	 * @return String[] Requirements
+	 */
+	public String[] getRequirements() {
+		return requirements;
+	}
+	/**
+	 * Sets the requirements of the badge as an array of strings
+	 * @param requirements The requirements to set
+	 */
+	public void setRequirements(String[] requirements) {
+		this.requirements = requirements;
+	}
+
+}
