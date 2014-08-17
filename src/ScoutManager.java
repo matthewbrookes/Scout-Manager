305618d392ccb4888d0b8260542e6dd8273f9bfe
Matthew Brookes <mbrookes1304@gmail.com>
2014-08-17 18:44:33 +0100

    Created home screen
    This is the first screen the user sees when they start the program. The
    routine of the home screen also serves to establish an SQLite database
    if one does not already exist and then create two tables (Badges and
    Scouts) if they do not exist.

--- src/ScoutManager.java
+++ src/ScoutManager.java
@@ -1,4 +1,11 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 /**
  * This is the main program for the Scout Manager project and creates the home
@@ -7,7 +14,10 @@ import java.sql.*;
  */
 public class ScoutManager {
 	/**
-	 * Main routine of program
+	 * Main routine of program which establishes a database if one does not
+	 * already exist and creates the Scouts and Badges tables if they do not
+	 * exist. Then draws the home screen which the user sees when the program
+	 * is started.
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -52,6 +62,10 @@ public class ScoutManager {
 			e1.printStackTrace();
 		} 
 		
+		final JFrame frame = new JFrame("Scout Manager"); //Root container
+		frame.setBounds(0, 0, 800, 600);
+		drawScreen(frame);
+		frame.setVisible(true); //Show window
 		
 	}
 	
@@ -72,5 +86,143 @@ public class ScoutManager {
 		    System.exit(0);
 	    }
 		return null;
+	}
+	
+	
+	/**
+	 * This constructor will be called when the home button is pressed at any stage during
+	 * the system. It will redraw the home screen on the existing frame.
+	 * @param frame The current window when home screen pressed
+	 */
+	ScoutManager(JFrame frame){
+		System.out.println("Home");
+		Container pane = frame.getContentPane();
+		pane.removeAll();
+		pane.repaint();
+		drawScreen(frame);
+		frame.setVisible(true);
+	}
+	/**
+	 * This method draws the header and body of the home screen
+	 * on the frame which is passed
+	 * @param frame The current window
+	 */
+	private static void drawScreen(final JFrame frame){
+		Container pane = frame.getContentPane(); //Main screen
+		pane.setBounds(0,0,800,100);
+		pane.setBackground(Color.WHITE); //Set the background to white
+		JPanel header = new JPanel(); //Header panel
+		header.setBounds(0, 0, 800, 100);
+		//Set box layout on header
+		BoxLayout headerLayout = new BoxLayout(header, BoxLayout.X_AXIS);
+		header.setLayout(headerLayout);
+		Box box = Box.createHorizontalBox();
+		
+		header.setBackground(new Color(139,0,102)); //Set purple color to background
+		//Create home and back buttons
+		ImageIcon back = new ImageIcon("./res/back.png");
+		JLabel backButton = new JLabel(back);
+		box.add(backButton);
+		
+		ImageIcon home = new ImageIcon("./res/home.png");
+		JLabel homeButton = new JLabel(home);
+		homeButton.addMouseListener(new MouseListener(){
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Return to main screen
+				new ScoutManager(frame);
+			}
+			
+			//Required methods but useless
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+		});
+		box.add(homeButton);
+		
+		//Add title
+		JLabel title = new JLabel("HOME");
+		title.setFont(new Font("Verdana",Font.PLAIN, 50));
+		title.setForeground(new Color(237,119,3));
+		box.add(Box.createHorizontalGlue());
+		box.add(title);
+		
+		//Add scout logo
+		ImageIcon scoutLogo = new ImageIcon("./res/scout_logo.png");
+		JLabel logo = new JLabel(scoutLogo);
+		logo.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		//Add to header
+		box.add(Box.createHorizontalGlue());
+		box.add(logo);
+		header.add(box);
+		
+		pane.add(header, BorderLayout.NORTH); //Add header to main screen
+		
+		//Create main body
+		JPanel body = new JPanel();
+		body.setBackground(Color.WHITE);
+		GridLayout bodyLayout = new GridLayout(2,3);
+		body.setLayout(bodyLayout);
+		body.setBorder(new EmptyBorder(20,20,80,200));
+		
+		//Create buttons
+		JButton reports = new JButton("Reports");
+		reports.setBackground(new Color(79,129,189));
+		reports.setFont(new Font("Calibri",Font.PLAIN,20));
+		reports.setForeground(Color.WHITE);
+		reports.setFocusable(false);
+		
+		//When button is clicked start Reports screen
+		reports.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Start Reports screen
+			}	
+		});
+		
+		JButton bManagement = new JButton("Badge Management");
+		bManagement.setFont(new Font("Calibri",Font.PLAIN,20));
+		bManagement.setForeground(Color.WHITE);
+		bManagement.setFocusable(false);
+		bManagement.setBackground(new Color(79,129,189));
+		
+		//When button is clicked start Badge Management screen
+		bManagement.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Start Badge Management screen
+			}	
+		});
+				
+		JButton sManagement = new JButton("Scout Management");
+		sManagement.setBackground(new Color(79,129,189));
+		sManagement.setFont(new Font("Calibri",Font.PLAIN,20));
+		sManagement.setForeground(Color.WHITE);
+		sManagement.setFocusable(false);
+		
+		//When button is clicked start Reports screen
+		sManagement.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Start Scout Management screen
+			}	
+		});
+				
+		//Set padding between buttons
+		bodyLayout.setHgap(50);
+		bodyLayout.setVgap(100);
+		
+		//Add buttons to body panel
+		body.add(reports);
+		body.add(bManagement);
+		body.add(sManagement);
+		
+		pane.add(body, BorderLayout.CENTER);
 	}
 }
